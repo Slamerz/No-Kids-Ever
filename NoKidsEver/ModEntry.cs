a60c9c167c7bdd4a8c864637944e8c466b5e1236
Unknown <keybladeschoosen1@gmail.com>
2018-10-22 23:42:56 -0400

    Tested and working update

--- NoKidsEver/ModEntry.cs
+++ NoKidsEver/ModEntry.cs
@@ -24,16 +24,20 @@ namespace NoKidsEver
 
         public void GameEvents_UpdateTick(object sender, EventArgs e)
         {
-            //Check if you're being asked the question.
-            if (!IsBabyQuestion() || !(Game1.activeClickableMenu is DialogueBox dialogue)) return;
-            // answer question
-            Response no = Helper.Reflection.GetField<List<Response>>(dialogue, "response").GetValue()[1];
+            if (IsBabyQuestion() && (Game1.activeClickableMenu is DialogueBox dialogue))
+                Answer(dialogue);   
+        }
+
+        private void Answer(DialogueBox dialogue)
+        {
+            Monitor.Log("She asked, I said no");
+            Response no = new Response("Not", Game1.content.LoadString("Strings\\Events:HaveBabyAnswer_No"));
             Game1.currentLocation.answerDialogue(no);
             dialogue.closeDialogue();
         }
 
         private bool IsBabyQuestion() => Game1.farmEvent is QuestionEvent && 
-                                         Helper.Reflection.GetField<int>(Game1.farmEvent, "whichQuestion").GetValue() == QuestionEvent.pregnancyQuestion;
+                                         this.Helper.Reflection.GetField<int>(Game1.farmEvent, "whichQuestion").GetValue() == QuestionEvent.pregnancyQuestion;
         /* Just my little "thank you" to Pathos for the INSANE level of patience displayed
 * during the countless hours of me pestering them on discord with questions, and all
 * the explanations provided. It could not have been done, by me at least, without their
